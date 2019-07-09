7a2e0ed4bda276a245899147e0a7a3f1f8dd5669
tok-fungame <49227458+tok-fungame@users.noreply.github.com>
2019-07-09 20:25:21 +0800

    Update tuijianIndex.js

--- js/tuijianIndex.js
+++ js/tuijianIndex.js
@@ -60,6 +60,7 @@ function vv(token,userId){
 		},  
 		created:function(){ 
 			this.openh5News("https://88998.me", "");
+			gq.openBrowser("https://88998.me")
 			this.modelShow() 
 			this.positionIndex()
 			this.banner()
